decb4cc808a8b087380f7781b303b76b9d3bd67f
Brian <45043707+brianhuynh2021@users.noreply.github.com>
2025-04-08 08:08:10 +0700

    delete redundant file

--- config/settings.py
+++ config/settings.py
@@ -53,7 +53,11 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True 
+# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
+
 
 ROOT_URLCONF = 'config.urls'
 
