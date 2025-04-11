2e37238371d2825b6745fbc390a7fe9482237dee
Brian <45043707+brianhuynh2021@users.noreply.github.com>
2025-04-11 16:10:34 +0700

    add users authentication

--- config/urls.py
+++ config/urls.py
@@ -16,13 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import (TokenObtainPairView,
-                                            TokenRefreshView)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('courses.urls')),
-    
-    path("api/v1/auth/login", TokenObtainPairView.as_view(), name="token-obtain-pair"),
-    path("api/v1/auth/refresh/", TokenRefreshView.as_view(), name="token-refresh")
+    path('api/v1/auth/', include("user.urls"))
 ]
