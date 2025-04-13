c4dad89f19e19f34aef04e916ad6e39011d49d0e
Brian <45043707+brianhuynh2021@users.noreply.github.com>
2025-04-13 16:25:27 +0700

    login-refresh-register apis

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from rest_framework_simplejwt.views import (TokenObtainPairView,
                                             TokenRefreshView)
-
+from views import RegisterView
 
 urlpatterns = [
     path("login/", TokenObtainPairView.as_view(), name="login"),
     path("token_refresh/", TokenRefreshView.as_view(), name="token-refresh"),
+    path("register/", RegisterView.as_view(), name='register')
 ]
